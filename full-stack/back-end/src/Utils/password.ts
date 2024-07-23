50107b6eb8e94ba7482e84d6c373075162f3f38f
Lukas <lukas.gudonis1989@gmail.com>
2024-07-23 15:57:56 +0300

    Pataisiau jwt tokeno generavima.

--- full-stack/back-end/src/Utils/password.ts
+++ full-stack/back-end/src/Utils/password.ts
@@ -1,9 +1,9 @@
 import jwt from "jsonwebtoken";
 import { Types } from "mongoose";
 
-const expiresIn = "30d";
+const expiresIn = "10d";
 
 export const generateToken = (payload: Types.ObjectId) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn });
+  const token = jwt.sign({id: payload.toString }, process.env.JWT_SECRET!, { expiresIn });
   return token;
 };
